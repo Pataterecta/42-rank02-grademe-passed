be2c53a26d29682cbfd5b79ffcc3ee3d0c0907ed
Pataterecta <140719296+Pataterecta@users.noreply.github.com>
2024-11-11 22:07:34 +0100

    Add files via upload

--- level02/print_bits.c
+++ level02/print_bits.c
@@ -14,14 +14,21 @@
 
 #include <unistd.h>
 
-void	print_bits(unsigned char octet)
+void    print_bits(unsigned char octet)
 {
     int i = 8;
-    unsigned char   bit;
+    unsigned char   result;
 
-    while (i--)
+    while(i--)
     {
-        bit = (octet >> i & 1) + '0';
-        write(1, &bit, 1);
+        result = (octet >> i & 1) + '0'; 
+        write(1, &result, 1);
     }
 }
+
+// int main(void)
+// {
+//     unsigned char    octet = 2;
+//     print_bits(octet);
+//     return (0);
+// }
